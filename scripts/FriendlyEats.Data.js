241c30bfc9e6b93ab08278d1668101ac73e9dd94
Seungyup Kwag <sykwag7@gmail.com>
2022-03-20 02:16:43 -0500

    addRestaurant

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -15,37 +15,36 @@
  */
 'use strict';
 
-FriendlyEats.prototype.addRestaurant = function(data) {
-  /*
-    TODO: Implement adding a document
-  */
+FriendlyEats.prototype.addRestaurant = function (data) {
+  var collection = firebase.firestore().collection('restaurants');
+  return collection.add(data);
 };
 
-FriendlyEats.prototype.getAllRestaurants = function(renderer) {
+FriendlyEats.prototype.getAllRestaurants = function (renderer) {
   /*
     TODO: Retrieve list of restaurants
   */
 };
 
-FriendlyEats.prototype.getDocumentsInQuery = function(query, renderer) {
+FriendlyEats.prototype.getDocumentsInQuery = function (query, renderer) {
   /*
     TODO: Render all documents in the provided query
   */
 };
 
-FriendlyEats.prototype.getRestaurant = function(id) {
+FriendlyEats.prototype.getRestaurant = function (id) {
   /*
     TODO: Retrieve a single restaurant
   */
 };
 
-FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
+FriendlyEats.prototype.getFilteredRestaurants = function (filters, renderer) {
   /*
     TODO: Retrieve filtered list of restaurants
   */
 };
 
-FriendlyEats.prototype.addRating = function(restaurantID, rating) {
+FriendlyEats.prototype.addRating = function (restaurantID, rating) {
   /*
     TODO: Retrieve add a rating to a restaurant
   */
